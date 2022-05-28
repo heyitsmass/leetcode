d8a33001272b8421190504eb7ed1231b6b58843f
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-05-27 20:09:58 -0700

    Solved problem 1 (Unoptimized)

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> tmp; 
+
+        for(int i = 0; i < nums.size(); i++){ 
+          for(int j = i+1; j < nums.size(); j++){ 
+            if (nums[i] + nums[j] == target) { 
+              tmp.push_back(i); 
+              tmp.push_back(j); 
+              return tmp; 
+            }
+          }
+        }
+      return tmp; 
+    }
+};
---
+++ 1.py
@@ -0,0 +1,9 @@
+class Solution:
+  def twoSum(self, nums: list, target: int) -> list:
+    
+    for i in range(len(nums)): 
+      for j in range(i+1, len(nums)): 
+        if nums[i] + nums[j] == target: 
+          return [i, j]  
+
+print(Solution().twoSum(nums = [3,3], target = 6)) 
