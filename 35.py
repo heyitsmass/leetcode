087d0da7ae7a2545dad1b86f6097e744827de95c
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-06-02 15:42:13 -0700

    Solved Problem 35

---
+++ 35.py
@@ -0,0 +1,16 @@
+class Solution:
+  def searchInsert(self, nums:list, target: int) -> int:
+    if target > nums[-1]: 
+      return len(nums) 
+
+    elif target < nums[0]: 
+      return 0;  
+    
+    elif target in nums: 
+      return nums.index(target) 
+
+    else: 
+      for i in range(len(nums)-1):  
+        if nums[i] < target and nums[i+1] > target: 
+          return i+1 
+
