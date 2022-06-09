15ba6826a9f5e40e268cab243408aa6c77e09fac
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-06-09 15:28:07 -0700

    Optimized Problem 125

--- python solutions/125.py
+++ python solutions/125.py
@@ -3,11 +3,8 @@ import re
 class Solution:
   def isPalindrome(self, s: str) -> bool:
     
-    tmp = re.split(r'([a-zA-Z0-9])', re.sub(r'[^a-zA-Z0-9]', '', s).lower())
-
-    tmp = list(filter(None, tmp)) 
-
-    print(tmp, tmp[::-1]) 
+    tmp = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
 
     return tmp == tmp[::-1]
+
 
