5aa8641f9a223bb15ce82813532936b0f7fd444c
Brandon <58617118+heyitsmass@users.noreply.github.com>
2022-06-20 03:01:58 -0700

    Solved Problem 2243

---
+++ python solutions/2243.py
@@ -0,0 +1,15 @@
+class Solution:
+  def digitSum(self, s: str, k: int) -> str:
+    if len(s) <= k: 
+      return s 
+
+    new = "" 
+
+    i = 0
+    while i < len(s): 
+
+      new += str(sum([int(num) for num in str(s[i:i+k])])) 
+
+      i+=k
+
+    return self.digitSum(new, k) 
