5b06144946660f0cf37ab2beae0948499c3509e1
Brandon <58617118+heyitsmass@users.noreply.github.com>
2022-06-20 01:53:38 -0700

    Relocated Problem 217

--- 217.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums:list) -> bool:
-        map = {}
-        for num in nums: 
-          if num in map.keys(): 
-            return True 
-          else: 
-            map[num] = 1 
